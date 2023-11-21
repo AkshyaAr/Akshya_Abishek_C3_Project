09c93c2cc4414ed69a83437da32c2ec7f17bcfb4
akshy” <akshyapoo27@gmail.com>
2023-11-21 12:43:31 +0530

    initial commit

---
+++ Restaurant_project -Student version/src/main/java/RestaurantService.java
@@ -0,0 +1,32 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+    public Restaurant findRestaurantByName(String restaurantName){
+        Restaurant searchRestaurant = null, foundRestaurant = null;
+        for (Restaurant restaurant : restaurants) {
+            searchRestaurant = restaurant;
+            if (searchRestaurant.getName().contains(restaurantName)) {
+                foundRestaurant = searchRestaurant;
+                break;
+            }
+        }
+        return foundRestaurant;
+    }
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
